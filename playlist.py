d38f8dcead3be47fec21305907f4e89ce16d2e8f
sj1224488 <shubha.jindal@ascte.org>
2026-09-22 09:21:50 -0500

    Add New Song
    Added A new song to the list of songs.

--- playlist.py
+++ playlist.py
@@ -4,6 +4,7 @@ SONGS = [
     ("Imagine", "John Lennon"),
     ("Blinding Lights", "The Weeknd"),
     ("Viva La Vida", "Coldplay"),
+    ("The Wheels On The Bus", "A Random Kid"),
 ]
 
 def print_playlist(songs):
